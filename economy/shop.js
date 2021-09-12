dd5d0a26d9024537d33d336a3940eb080e34e7e3
Andres Emmanueñ <88838298+Andres-Emma@users.noreply.github.com>
2021-09-12 13:29:12 -0500

    Create shop.js

---
+++ economy/shop.js
@@ -0,0 +1,39 @@
+module.exports = {
+  
+  name: "shop",
+  code: `$author[Tienda;$userAvatar[884561627700146226]] 
+$description[**> Necesidad:**
+
+Pizza🍕
+**$getServerVar[prefix]buy-pizza** = $95
+**$getServerVar[prefix]eat-pizza** = Si tienes hambre, cómelo. +2 Salud, -6 Hambriento, -4 Sediento.
+
+Drink🥛
+**$getServerVar[prefix]buy-drink** = $45
+**$getServerVar[prefix]drink** = Si tiene sed, bébalo. +3 Salud, -2 Hambriento, -5 Sediento.
+
+Medicine💊
+**$getServerVar[prefix]buy-medic** = $125
+**$getServerVar[prefix]take-medicine** = Cuando esté enfermo, úselo. +9 Salud, -4 Hambriento, -4 Sediento.
+
+**> Item :**
+
+Fishing equipment🎣
+**$getServerVar[prefix]buy-fishrod** = $75
+
+Laptop🖱
+**$getServerVar[prefix]buy-laptop** = $13.000
+
+Car🚗
+**$getServerVar[prefix]buy-car** = $65.000
+
+Fuel⛽
+**$getServerVar[prefix]buy-fuel** = $2500/1L
+
+House🏡
+**$getServerVar[prefix]buy-house** = $2.000.000
+]
+$color[RANDOM]
+$footer[Creado por ANDRES-EMMA]
+$addTimestamp`
+}
